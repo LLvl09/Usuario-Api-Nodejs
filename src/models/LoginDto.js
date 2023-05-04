2dbf93512b9d487eaf98cc83d403b766df34885e
Lucas  Luan <97856858+LLvl09@users.noreply.github.com>
2023-05-04 15:48:55 -0300

    Delete LoginDto.js

--- src/models/LoginDto.js
+++
@@ -1,8 +0,0 @@
-class Login  {
-    constructor(value) {
-        this.email = value.email != null ? value.email: '';
-        this.password = value.password != null ? value.password: '';
-    }
-}
-
-module.exports = Login;
